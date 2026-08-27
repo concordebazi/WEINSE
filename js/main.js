e422e4f460474017945b57c530b578202a74f4ce
concorde bazimaziki <59648271+concordebazi@users.noreply.github.com>
2026-08-27 12:20:59 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// ==========================================
+// WEINSE MOBILE NAVIGATION
+// ==========================================
+
+const menuToggle =
+document.getElementById("menu-toggle");
+
+const mainNav =
+document.getElementById("main-nav");
+
+
+if (menuToggle && mainNav) {
+
+    menuToggle.addEventListener(
+        "click",
+        function () {
+
+            mainNav.classList.toggle("active");
+
+        }
+    );
+
+}
